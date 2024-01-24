84ab1ac29df5102367e4093a6019ca1a873dacad
asma shahzadi <asma786shahzadi@gmail.com>
2024-01-24 16:29:21 +0500

    update train.py

--- training/train.py
+++ training/train.py
@@ -1,12 +1,15 @@
+from ctypes import resize
 import os, sys
 sys.path.append('.')
 
 import torch 
 import torch.nn as nn
 from torchvision import transforms
+from torch.utils.data import DataLoader
+from data import test
 
 import config
-from tools.iris_dataset import * # transformers
+
 from tools.logger import Logger
 from nets.optimized_landmark import LNet
 from training.trainer import Trainer
@@ -16,28 +19,31 @@ from checkpoint import CheckPoint
 use_cuda = config.USE_CUDA and torch.cuda.is_available()
 device = torch.device("cuda:0" if use_cuda else "cpu")
 
-transform = transforms.Compose([Resize((int(1.4*config.IMAGE_WIDTH), 
-                                        int(1.4*config.IMAGE_HEIGHT))),
-                                Rescale(3, 15),
-                                RandomFlip(0.4),
-                                RandomGaussianBlur(0.6), 
-                                RandomMedianBlur(0.6), 
-                                # RandomMotionBlur(20),
-                                RandomCropResize((config.IMAGE_WIDTH, 
-                                                  config.IMAGE_HEIGHT), 0.2),
-                                ToTensor(),
-                                Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])]
-                                )
+transform = transforms.Compose([
+    transforms.Resize((int(1.2 * config.IMAGE_WIDTH), int(1.2 * config.IMAGE_HEIGHT))),
+    transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.2),
+    transforms.RandomHorizontalFlip(p=0.5),
+    transforms.RandomVerticalFlip(p=0.5),
+    transforms.RandomRotation(degrees=15),
+    transforms.RandomResizedCrop((config.IMAGE_HEIGHT, config.IMAGE_WIDTH), scale=(0.8, 1.2), ratio=(0.9, 1.1)),
+    transforms.GaussianBlur(kernel_size=3),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
+])
 
-train_loader = torch.utils.data.DataLoader(
-    FaceLandmarkDataset([{'root_dir': config.TRAIN_DATA_DIR,
-                         'label_file': config.LANDMARKS_ANNO_FILE}],
-                         point_num=config.NUM_LANDMARKS,
-                         transform=transform),
-    batch_size = config.batch_size,
-    num_workers = config.num_threads,
-    shuffle=True)
 
+train_dataset = test(
+    [{'root_dir': config.TRAIN_DATA_DIR, 'label_file': config.LANDMARKS_ANNO_FILE}],
+    point_num=config.NUM_LANDMARKS,
+    transform=transform
+)
+
+train_loader = DataLoader(
+    train_dataset,
+    batch_size=config.batch_size,
+    num_workers=config.num_threads,
+    shuffle=True
+)
 model = LNet()
 model.load_state_dict(torch.load('result/iris_lnet/check_point/32_landmarks_model_200.pth'))
 if torch.cuda.device_count() > 1:
@@ -48,7 +54,6 @@ model.to(device)
 lossfn = nn.MSELoss()
 checkpoint = CheckPoint(config.save_path)
 optimizer = torch.optim.Adam(model.parameters(), lr=config.learning_rate)
-# optimizer = torch.optim.SGD(model.parameters(), lr=config.learning_rate)
 scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=config.step, gamma=0.1)
 
 logger = Logger(config.save_path)
